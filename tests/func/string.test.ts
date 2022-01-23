16e42018055b65da5991af3b6671a0398e68571c
jonathan <contact@grafikart.fr>
2022-01-23 02:05:32 +0100

    fix: wording test

--- tests/func/string.test.ts
+++ tests/func/string.test.ts
@@ -2,25 +2,28 @@ import { it, describe, expect, assert } from "vitest";
 import { wordProximity } from "../../src/func/string";
 
 describe("wordProximity", () => {
-  [
-    ["niche", "chien", 4],
-    ["maison", "maçon", 2],
-    ["ab", "ac", 1],
-    ["ac", "bc", 1],
-    ["abc", "axc", 1],
-    ["xabxcdxxefxgx", "1ab2cd34ef5g6", 6],
-    ["xabxcdxxefxgx", "abcdefg", 6],
-    ["javawasneat", "scalaisgreat", 7],
-    ["example", "samples", 3],
-    ["forward", "drawrof", 6],
-    ["sturgeon", "urgently", 6],
-    ["levenshtein", "frankenstein", 6],
-    ["distance", "difference", 5],
-    ["distance", "eistancd", 2],
-  ].forEach((test) => {
-    const [a, b, expected] = test;
-    it(`Distance between "${a}" and ${b} should be ${expected}`, () => {
-      assert.equal(wordProximity(a, b), expected);
+  it("should find the distance between 2 words", () => {
+    [
+      ["niche", "chien", 4],
+      ["maison", "maçon", 2],
+      ["ab", "ac", 1],
+      ["ac", "bc", 1],
+      ["abc", "axc", 1],
+      ["xabxcdxxefxgx", "1ab2cd34ef5g6", 6],
+      ["xabxcdxxefxgx", "abcdefg", 6],
+      ["javawasneat", "scalaisgreat", 7],
+      ["example", "samples", 3],
+      ["forward", "drawrof", 6],
+      ["sturgeon", "urgently", 6],
+      ["levenshtein", "frankenstein", 8],
+      ["distance", "difference", 5],
+      ["distance", "eistancd", 2],
+    ].forEach(([a, b, expected]) => {
+      assert.equal(
+        wordProximity(a, b),
+        expected,
+        `distance between "${a}" and "${b}"`
+      );
     });
   });
 });
